25458d8ad33afc8c1322e9219ab4d14c7f179d32
Andrew Morrison <morro141@gmail.com>
2016-11-20 22:18:22 -0500

    Add checkbox

--- views/index.jsx
+++ views/index.jsx
@@ -28,23 +28,34 @@ class TodoList extends React.Component {
 	}
 }
 
+class Todo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {checked: false};
+	}
+
+	handleChange(e) {
+		this.setState({checked: e.target.checked})
+	}
+	render() {
+		return (
+			<tr>
+				<td style={{border: '1px solid black'}}>
+					<input type='checkbox' checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+				</td>
+				<td style={{border: '1px solid black'}}>{this.props.title}</td>
+				<td style={{border: '1px solid black'}}>{this.props.children}</td>
+			</tr>
+		);
+	}
+}
+
 class TodoForm extends React.Component {
 	render() {
 		return (
 			<div className='todoForm'>
 				I am a TodoForm.
 			</div>
-		);
-	}
-}
-
-class Todo extends React.Component {
-	render() {
-		return (
-			<tr>
-				<td style={{border: '1px solid black'}}>{this.props.title}</td>
-				<td style={{border: '1px solid black'}}>{this.props.children}</td>
-			</tr>
 		);
 	}
 }
